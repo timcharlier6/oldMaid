1d9e3a3a8f9d7e25ebfd9559e60d5f9705d05c69
Tim <128478437+timcharlier6@users.noreply.github.com>
2024-04-27 13:27:48 +0200

    feat: message and style

--- c.mjs
+++ c.mjs
@@ -8,9 +8,7 @@ const rl = readline.createInterface({
     output: process.stdout
 });
 
-//const suits = ['\u2665', '\u2666', '\u2663', '\u2660']; // Hearts, Diamonds, Clubs, Spades
-//const suits = ['\u2661', '\u2662', '\u2667', '\u2664']; // Hearts, Diamonds, Clubs, Spades (Different styles for French)
-console.log("SELECT:");
+console.log(chalk.yellow("SELECT:"));
 
 const selectPromise = new Promise((resolve, reject) => {
     select(
@@ -103,14 +101,14 @@ const discardPairs = (playerHand, computerHand, language) => {
     let dirtyHand = [...playerHand];
     let dirtyComputerHand = [...computerHand];
 
-    console.log(en ? '\n╭─────────────────────────────────────╮\n│ You should remove all your pairs... │\n╰─────────────────────────────────────╯\n' : '\n╭─────────────────────────────────────────╮\n│ Vous devez enlevez toutes vos paires... │\n╰─────────────────────────────────────────╯\n');
+    console.log(en ? chalk.yellow('\n╭─────────────────────────────────────╮\n│ You should remove all your pairs... │\n╰─────────────────────────────────────╯\n') : chalk.yellow('\n╭─────────────────────────────────────────╮\n│ Vous devez enlevez toutes vos paires... │\n╰─────────────────────────────────────────╯\n'));
 
     const promptPairSelection = () => {
         inquirer.prompt([
             {
                 type: 'checkbox',
                 name: 'selectedCards',
-                message: en ? 'Select two pairs to discard:' : 'Selectionnez deux pairs a defausser:',
+                message: en ? chalk.cyan('Select two pairs to discard:') : chalk.cyan('Selectionnez deux pairs a defausser:'),
                 choices: dirtyHand,
                 validate: validatePairSelection
             }
@@ -119,7 +117,7 @@ const discardPairs = (playerHand, computerHand, language) => {
 
     const validatePairSelection = (input) => {
         if (input.length !== 2 || input[0].split(" ")[0] !== input[1].split(" ")[0]) {
-            return en ? 'Please select a pair.' : 'Veuillez selectionner une pair.';
+            return en ? chalk.red('Please select a pair.') : chalk.red('Veuillez selectionner une pair.');
         } else {
             return true;
         }
@@ -133,10 +131,10 @@ const discardPairs = (playerHand, computerHand, language) => {
         const pairsRemaining = countPairs(dirtyHand);
 
         if (pairsRemaining.length > 0) {
-            en ? console.log(`You have ${pairsRemaining.length / 2} pairs remaining.`) : console.log(`Il vous reste ${pairsRemaining.length / 2 } pairs.`);
+            en ? console.log(chalk.yellow(`You have ${pairsRemaining.length / 2} pairs remaining.`)) : console.log(chalk.yellow(`Il vous reste ${pairsRemaining.length / 2 } pairs.`));
             promptPairSelection();
         } else {
-            en ? console.log("No pairs left.") : console.log("Plus de pairs.");
+            en ? console.log(chalk.green("No pairs left.")) : console.log(chalk.green("Plus de pairs."));
         }
     };
 
@@ -144,9 +142,8 @@ const discardPairs = (playerHand, computerHand, language) => {
     dirtyComputerHand = filterHand(dirtyComputerHand);
 
     const removedCards = computerHand.length - dirtyComputerHand.length;
-    console.log(en ? `\nNumber of cards removed from computer's hand: ${removedCards}` : `\nNombre de cartes retirées de la main de l'ordinateur : ${removedCards}`);
-    console.log(computerHand);
-    console.log(dirtyComputerHand);
+    console.log(en ? chalk.yellow(`\nComputer has removed ${removedCards / 2} pairs from his hand, it remains ${dirtyComputerHand.length} cards.`) : chalk.yellow(`\nL'ordinateur a defausse ${removedCards / 2} de sa main, il lui reste ${dirtyComputerHand.length} cartes.`));
+    console.log(computerHand + '\n' + dirtyComputerHand);
 
     promptPairSelection();
 };
@@ -197,5 +194,4 @@ function filterHand(hand, filteredHand = [], index = 0) {
     // Pass both player's and computer's hands to discardPairs function
     discardPairs(oldMaid.hands[0], oldMaid.hands[1], selectedLanguage);
 })();
-
 
